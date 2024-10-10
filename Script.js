a8315f69d32042fe8a519bb1c667afd5229aec67
Muhammad Huzaifa Nadeem <f2020266384@umt.edu.pk>
2024-10-10 06:12:10 -0400

    Understand the working of Reducer and Action in redux

--- Script.js
+++ Script.js
@@ -1,13 +1,22 @@
-let state = {
-  count: 0,
+let reduxState = {
+  post: 0,
   name: "Ali",
   age: 13,
 };
 
-function reducer(state) {
-  return { ...state, count: state.count + 1 };
+function reducer(state, action) {
+  if (action.type === "post/increase") {
+    return { ...state, post: state.post + 1 };
+  } else if (action.type === "post/decrease") {
+    return { ...state, post: state.post - 1 };
+  }
+  return state;
 }
 
 // What Redux Will Do
-
-reduxState = reducer(reduxState);
+reduxState = reducer(reduxState, { type: "post/increase" });
+console.log(reduxState);
+reduxState = reducer(reduxState, { type: "post/increase" });
+console.log(reduxState);
+reduxState = reducer(reduxState, { type: "post/decrease" });
+console.log(reduxState);
