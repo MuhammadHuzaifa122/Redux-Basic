a742af96f758ad61e7d54c22d33f9bbe1e175300
Muhammad Huzaifa Nadeem <f2020266384@umt.edu.pk>
2024-10-10 05:15:33 -0400

    Learn how to update the state without mutating it

--- Script.js
+++ Script.js
@@ -1,10 +1,16 @@
 let state = {
   count: 0,
 };
+
+let previousState = state;
+
 function increment() {
   // mutating state which is not recommended in Redux
   //   state.count = state.count + 1;
+  // Not Mutating the state
+  state = { count: state.count + 1 };
 }
+
 increment();
 console.log(state);
 increment();
