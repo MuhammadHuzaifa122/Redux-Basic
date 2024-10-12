78527ff1ccc367f4154d4a8b1da8ef0563efe8c8
Muhammad Huzaifa Nadeem <f2020266384@umt.edu.pk>
2024-10-12 06:00:07 -0400

    Learn the use of subscribe method

--- Script.js
+++ Script.js
@@ -13,13 +13,19 @@ function reducer(state = initialState, action) {
     return { ...state, post: state.post - 1 };
   } else if (action.type === "post/increaseBy") {
     return { ...state, post: state.post + action.payload };
+  } else if (action.type === "post/decreaseBy") {
+    return { ...state, post: state.post - action.payload };
   }
   return state;
 }
 
 const store = createStore(reducer);
 console.log(store);
-console.log(store.getState());
 
+store.subscribe(() => {
+  console.log(store.getState());
+});
 store.dispatch({ type: "post/increase" });
-console.log(store.getState());
+store.dispatch({ type: "post/decrease" });
+store.dispatch({ type: "post/increaseBy", payload: 5 });
+store.dispatch({ type: "post/decreaseBy", payload: 3 });
