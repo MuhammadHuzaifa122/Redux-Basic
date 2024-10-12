0d6f95018c4e66205f11ff46089e68106055c6e2
Muhammad Huzaifa Nadeem <f2020266384@umt.edu.pk>
2024-10-12 05:34:44 -0400

    Learn the difference between getState and dispatch methods

--- Script.js
+++ Script.js
@@ -1,14 +1,12 @@
 import { createStore } from "redux";
 
-console.log(createStore);
-
-let reduxState = {
+const initialState = {
   post: 0,
   name: "Ali",
   age: 13,
 };
 
-function reducer(state, action) {
+function reducer(state = initialState, action) {
   if (action.type === "post/increase") {
     return { ...state, post: state.post + 1 };
   } else if (action.type === "post/decrease") {
@@ -19,14 +17,9 @@ function reducer(state, action) {
   return state;
 }
 
-// What Redux Will Do
-reduxState = reducer(reduxState, { type: "post/increase" });
-console.log(reduxState);
-reduxState = reducer(reduxState, { type: "post/increase" });
-console.log(reduxState);
-reduxState = reducer(reduxState, { type: "post/decrease" });
-console.log(reduxState);
-reduxState = reducer(reduxState, { type: "post/increaseBy", payload: 10 });
-console.log(reduxState);
-reduxState = reducer(reduxState, { type: "post/increaseBy", payload: 10 });
-console.log(reduxState);
+const store = createStore(reducer);
+console.log(store);
+console.log(store.getState());
+
+store.dispatch({ type: "post/increase" });
+console.log(store.getState());
