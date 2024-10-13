68dbbd40a411e1ea6b9bd494fecf6c5d69bbd07b
Muhammad Huzaifa Nadeem <f2020266384@umt.edu.pk>
2024-10-13 05:42:26 -0400

    Create my own redux using basic functionality

--- Script.js
+++ Script.js
@@ -1,48 +1,62 @@
 import { createStore } from "redux";
-
-const post = document.querySelector(".post-count");
+import { myCreateStore } from "./my-redux";
+const postCountElement = document.querySelector(".post-count");
 
 const initialState = {
   post: 0,
-  name: "Ali",
-  age: 13,
+  name: "Anurag Singh",
+  age: 26,
 };
 
-const INCREASE = "post/increase";
-const DECREASE = "post/decrease";
-const INCREASE_BY = "post/inreaseBy";
+const INCREMENT = "post/increment";
+const DECREMENT = "post/decrement";
+const INCREASE_BY = "post/increaseBy";
 const DECREASE_BY = "post/decreaseBy";
 
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case INCREASE:
+    case INCREMENT:
       return { ...state, post: state.post + 1 };
-    case DECREASE:
+    case DECREMENT:
       return { ...state, post: state.post - 1 };
     case INCREASE_BY:
       return { ...state, post: state.post + action.payload };
     case DECREASE_BY:
       return { ...state, post: state.post - action.payload };
-
     default:
       return state;
   }
 }
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.());
-console.log(store);
+const myStore = myCreateStore(reducer);
 
-store.subscribe(() => {
-  console.log(store.getState());
-  post.innerText = store.getState().post;
+// console.log(store);
+console.log(myStore);
+
+const unsubscribe1 = myStore.subscribe(() => {
+  console.log(myStore.getState());
+  postCountElement.innerText = myStore.getState().post;
 });
 
-post.innerText = store.getState().post;
-store.dispatch({ type: INCREASE });
-store.dispatch({ type: DECREASE });
-store.dispatch({ type: INCREASE_BY, payload: 15 });
-store.dispatch({ type: DECREASE_BY, payload: 3 });
+const unsubscribe2 = myStore.subscribe(() => {
+  console.log("hii");
+});
 
-setTimeout(() => {
-  store.dispatch({ type: DECREASE });
-}, 2000);
+const unsubscribe3 = myStore.subscribe(() => {
+  console.log("hello");
+});
+
+postCountElement.innerText = myStore.getState().post;
+
+myStore.dispatch({ type: INCREMENT });
+
+unsubscribe2();
+unsubscribe3();
+myStore.dispatch({ type: DECREMENT });
+myStore.dispatch({ type: INCREASE_BY, payload: 15 });
+myStore.dispatch({ type: DECREASE_BY, payload: 5 });
+
+postCountElement.addEventListener("click", () => {
+  myStore.dispatch({ type: INCREMENT });
+});
