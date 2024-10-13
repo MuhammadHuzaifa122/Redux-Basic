7df8a32852f4da388738ed47e882f1d3d9a6e706
Muhammad Huzaifa Nadeem <f2020266384@umt.edu.pk>
2024-10-13 06:19:37 -0400

    Add items to our cart

--- Script.js
+++ Script.js
@@ -1,12 +1,43 @@
 import { createStore } from "redux";
 import { productsList } from "./productsList";
+
 const initialState = {
   products: productsList,
   cartItems: [],
   wishList: [], // We can add only product id in wishlist
 };
 
-function reducer(state = initialState, action) {}
+const cartAddItems = "cart/addItem";
+const cartRemoveItems = "cart/RemoveItem";
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case cartAddItems:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload],
+      };
+    default:
+      return state;
+  }
+}
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__?.());
+
 console.log(store);
+
+store.dispatch({
+  type: "cart/addItem",
+  payload: { productId: 1, quantity: 1 },
+});
+store.dispatch({
+  type: "cart/addItem",
+  payload: { productId: 12, quantity: 1 },
+});
+store.dispatch({
+  type: "cart/addItem",
+  payload: { productId: 15, quantity: 1 },
+});
+store.dispatch({
+  type: "cart/addItem",
+  payload: { productId: 6, quantity: 1 },
+});
