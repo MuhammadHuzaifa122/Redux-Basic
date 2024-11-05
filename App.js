0a37f9cd7e1311f7ae4066113d25fad705249441
Muhammad Huzaifa Nadeem <f2020266384@umt.edu.pk>
2024-11-05 06:22:57 -0500

    install react-redux

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import React from "react";
 import productsList from "./store/productsList";
 import Product from "./components/Product";
-import { store } from "./store/index.js";
 import "./App.css";
 export default function App() {
-  console.log(store);
-
   return (
     <div className="products-container">
       {productsList.map(({ id, title, rating, price, image }) => (
