54d6bf844f854dfeb68006d3aa1ad5e4aff94807
Muhammad Huzaifa Nadeem <f2020266384@umt.edu.pk>
2024-11-05 06:27:37 -0500

    Learn about useSelector in redux

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from "react";
-import productsList from "./store/productsList";
 import Product from "./components/Product";
 import "./App.css";
+import { useSelector } from "react-redux";
 export default function App() {
+  const productsList = useSelector((state) => state.products);
+
   return (
     <div className="products-container">
       {productsList.map(({ id, title, rating, price, image }) => (
