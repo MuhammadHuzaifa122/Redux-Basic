46bdfa0fb09e9403ccf817a4514f076ae9b3abed
Muhammad Huzaifa Nadeem <f2020266384@umt.edu.pk>
2024-11-06 05:39:33 -0500

    Fix the CSS in our cart item page

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
 import React from "react";
-import Product from "./components/Product";
-import "./App.css";
-import { useSelector } from "react-redux";
-export default function App() {
-  //Use Selector is used to get the data from the store and comes in react redux
-  const productsList = useSelector((state) => state.products);
+import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
 
+import "./App.css";
+
+export default function App() {
   return (
-    <div className="products-container">
-      {productsList.map(({ id, title, rating, price, image }) => (
-        <Product
-          key={id}
-          title={title}
-          rating={rating.rate}
-          price={price}
-          imageUrl={image}
-        />
-      ))}
-    </div>
+    <>
+      <Header />
+      <Outlet />
+    </>
   );
 }
